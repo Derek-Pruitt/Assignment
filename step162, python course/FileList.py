c0717a1d5342eec19fe659fe9da2cb6223d8cd6d
Derekhunterrc0001 <64759619+Derekhunterrc0001@users.noreply.github.com>
2020-07-01 16:59:32 -0600

    update

--- step162, python course/FileList.py
+++ step162, python course/FileList.py
@@ -4,6 +4,16 @@
 
 # Assignment for school
 
+
+
+file_List = ('information.docx','Hello.txt','myImage.png',\
+             'myMovie.mpg','World.txt','data.pdf','myPhoto.jpeg')
+for value in file_List:
+    if value.endswith(".txt"):
+        print(value)
+
+
+        
 import sqlite3
 
 conn = sqlite3.connect('Step162.db')
